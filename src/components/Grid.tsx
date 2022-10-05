6dcc078735137d546cc1f421f629f3b43027a225
Jose Roberto <jrobertofonsecajr@gmail.com>
2022-10-04 23:59:33 -0300

    add forwardRef

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Tile from "./Tile";
 
 type Props = {};
@@ -6,13 +6,23 @@ const width = 16;
 const height = 8;
 
 const Grid = (props: Props) => {
+  const childRef = useRef<any>(null);
+
+  function handleTeste() {
+    childRef?.current?.playCallback();
+  }
+
   return (
     <div className="grid">
+      <button onClick={handleTeste}>Teste</button>
       {Array.from(Array(height).keys()).map((i) => (
         <>
-          {Array.from(Array(width).keys()).map((j) => (
-            <Tile />
-          ))}
+          {Array.from(Array(width).keys()).map((j) => {
+            if (i === 0 && j === 0) {
+              return <Tile ref={childRef} />;
+            }
+            return <Tile />;
+          })}
         </>
       ))}
     </div>
--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,11 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from "react";
 import * as Tone from "tone";
 
 type Props = {};
 
-const Tile = (props: Props) => {
+const Tile = forwardRef((props: Props, ref) => {
   const [isActive, setIsActive] = useState(false);
   const synth = new Tone.Synth().toDestination();
+
+  useImperativeHandle(ref, () => ({
+    playCallback() {
+      synth.triggerAttackRelease("C4", "8n");
+    },
+  }));
 
   useEffect(() => {
     if (isActive) {
@@ -22,6 +33,6 @@ const Tile = (props: Props) => {
   return (
     <div className={`tile ${isActive && "active"}`} onClick={handleClick}></div>
   );
-};
+});
 
 export default Tile;
